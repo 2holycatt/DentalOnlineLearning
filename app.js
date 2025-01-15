847dbccc001375775b641bcb64bdf603ea49abfe
999tttt <145903314+999tttt@users.noreply.github.com>
2025-01-16 00:44:45 +0700

    commit more

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const passport = require('passport');
 const PORT = process.env.PORT || 4000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/elearning";
 
-// const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo');
 // const authRouter = require('./routes/auth');
 
 const app = express();
