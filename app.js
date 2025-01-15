70b195b0392ec4a20060f6d932c90556ff0b24ab
999tttt <145903314+999tttt@users.noreply.github.com>
2025-01-16 04:15:01 +0700

    Fix to deploy

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const passport = require('passport');
 const PORT = process.env.PORT || 4000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/elearning";
 
-const MongoStore = require('connect-mongo');
+// const MongoStore = require('connect-mongo');
 // const authRouter = require('./routes/auth');
 
 const app = express();
@@ -114,17 +114,9 @@ app.use('/pdfs', express.static('uploads'));
 app.use(flash());
 app.use(session({
     secret: "ppw.smw_094",
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ 
-      mongoUrl: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/elearning",
-      ttl: 24 * 60 * 60 // หมดอายุใน 1 วัน
-    }),
-    cookie: {
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 24 * 60 * 60 * 1000 // 1 วัน
-    }
-  }));
+    resave: true,
+    saveUninitialized: true
+}));
 
 // custom middleware for login
 // const ifNotLoggedIn = (req, res, next) => {
--- controller/Login.js
+++ controller/Login.js
@@ -16,8 +16,8 @@ const axios = require('axios');
 
 const YOUR_CLIENT_ID = "95141771976-u1v2rj3o8ulagvqsrkgondmq8m4lou9t.apps.googleusercontent.com";
 const YOUR_CLIENT_SECRET = "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";
-const YOUR_REDIRECT_URL = 'http://localhost:4000/auth/google/callback'
-// const YOUR_REDIRECT_URL = 'https://dentalonlinelearning.onrender.com/auth/google/callback'
+// const YOUR_REDIRECT_URL = 'http://localhost:4000/auth/google/callback'
+const YOUR_REDIRECT_URL = 'https://dentalonlinelearning.onrender.com/auth/google/callback'
 
 const authGoogle = async (req, res) => {
     const scope = encodeURIComponent('profile email');
